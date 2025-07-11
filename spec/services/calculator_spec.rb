f9e1cd26f132d050e823e12709e37ba5ed4e2cc1
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2025-07-11 15:57:25 +1000

    test: .validate_input

--- spec/services/calculator_spec.rb
+++ spec/services/calculator_spec.rb
@@ -25,4 +25,23 @@ RSpec.describe "Calculator" do
       expect(final_balance).to eq(10330)
     end
   end
+
+  describe ".validate_input!" do
+    it "raises an error when invalid" do
+       params = { initial_deposit: 10000, interest_rate: 1.10, term: 3, vesting: "invalid" }
+
+      expect { Calculator.validate_input!(params) }.to raise_error(ArgumentError)
+    end
+
+    it "returns the validated input" do
+    input =  Calculator.validate_input!(initial_deposit: 10000, interest_rate: 1.10, term: 3, vesting: "maturity")
+
+
+      expect(input).to be_valid
+      expect(input.initial_deposit).to eq(10000)
+      expect(input.interest_rate).to eq(1.10)
+      expect(input.term).to eq(3)
+      expect(input.vesting).to eq("maturity")
+    end
+  end
 end
