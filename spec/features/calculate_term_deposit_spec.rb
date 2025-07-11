8778d5371e8653e85b06aa913c47199f9ce519ba
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2025-07-11 13:54:35 +1000

    test: pending feature spec for calculating term deposit

---
+++ spec/features/calculate_term_deposit_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Calculate term deposit" do
+  context "a user provides valid params" do
+    it "returns the correct final balance for monthly vesting" do
+      pending "Implement the DepositCalculator"
+      final_balance = DepositCalculator.calculate(1000, 1.10, 3, "monthly")
+
+      expect.(final_balance).to eq(10335)
+    end
+
+    it "returns the correct final balance for quarterly vesting" do
+      pending "Implement the DepositCalculator"
+      final_balance = DepositCalculator.calculate(1000, 1.10, 3, "quarterly")
+
+      expect.(final_balance).to eq(10335)
+    end
+    it "returns the correct final balance for annual vesting" do
+      pending "Implement the DepositCalculator"
+      final_balance = DepositCalculator.calculate(1000, 1.10, 3, "annually")
+
+      expect.(final_balance).to eq(10334)
+    end
+    it "returns the correct final balance for vesting at maturity" do
+      pending "Implement the DepositCalculator"
+      final_balance = DepositCalculator.calculate(1000, 1.10, 3, "maturity")
+
+      expect.(final_balance).to eq(10330)
+    end
+  end
+end
